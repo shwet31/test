915b4c4e3a5d2433189b3fee377d2ed6f92a9643
shwet31 <shwetank.ec.31@gmail.com>
2021-09-26 10:15:34 +0530

    modified test.py

--- test.py
+++ test.py
@@ -2,7 +2,8 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('-start','-a')
 parser.add_argument('-end','-b')
-print(a,b)
+args = parser.parse_args()
+print(args.a,args.b)
 
 print('Hello World')
 print('hello Shwetank')
