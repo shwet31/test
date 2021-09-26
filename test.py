bb603c13147304dc9307868967e5fdacb40cd072
shwet31 <shwetank.ec.31@gmail.com>
2021-09-26 10:10:32 +0530

    modified test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument('a','-start')
-parser.add_argument('b','-end')
+parser.add_argument('-start','-a')
+parser.add_argument('-end','-b')
 print(a,b)
 
 print('Hello World')
